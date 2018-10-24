47927e641e934a0f68f6f876e272bd4060a22b15
bhjeon76 <bhjeon@shmobile.co.kr>
2018-10-24 15:10:14 +0900

    Initial Commit

---
+++ CmdLineCtrlSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CmdLineCtrlSample
+//
+//  Created by 전병학 on 24/10/2018.
+//  Copyright © 2018 전병학. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CmdLineCtrlSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CmdLineCtrlSample
+//
+//  Created by 전병학 on 24/10/2018.
+//  Copyright © 2018 전병학. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
