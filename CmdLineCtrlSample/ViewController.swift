1f25560082658629dc448514f10d0f9ba3e88e5b
bhjeon76 <bhjeon@shmobile.co.kr>
2018-10-25 19:30:59 +0900

    testing execute process

--- CmdLineCtrlSample/ViewController.swift
+++ CmdLineCtrlSample/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: NSViewController {
     
     @objc dynamic var isRunning = false
     var outputPipe:Pipe!
-    var buildTask:Process!
+    var excuteTask:Process!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,11 +40,11 @@ class ViewController: NSViewController {
             
             let commandLineScopeLocation = commandLineScopeURL.path
             let excutePath = commandLineScopeLocation
-            let excuteFileName = "commandLineSope"
+            let excuteFileName = "./CommandLineSope"
             
             var arguments:[String] = []
-            arguments.append(excutePath)
-            arguments.append(excuteFileName)
+            //arguments.append(excutePath)
+            //arguments.append(excuteFileName)
             arguments.append(controlArgumentOption.stringValue)
             
             print("arguments = \(arguments)")
@@ -52,9 +52,11 @@ class ViewController: NSViewController {
             excuteButton.isEnabled = false
             spinner.startAnimation(self)
             
-            isRunning = true
+            runScript(arguments)
+            
         }
     }
+    
     
     @IBAction func cancelButton(_ sender: Any) {
         
@@ -64,5 +66,70 @@ class ViewController: NSViewController {
         }
     }
     
+    func runScript(_ arguments:[String]) {
+        
+        isRunning = true
+        let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+
+        taskQueue.async {
+            
+//            guard let path = Bundle.main.path(forResource: "ExcuteScript",ofType:"command") else {
+//
+//                print("Unable to locate ExcuteScript.command")
+//                let path = Bundle.main.path(forResource: "ExcuteScript",ofType:"command")
+//                print("path = \(path)")
+//
+//                return
+//            }
+            
+            self.excuteTask = Process()
+            self.excuteTask.launchPath = "/Users/bhjeon/CmdLineCtrlSample/CommandLineScope" //  /bin/ls" //path
+            self.excuteTask.arguments = [] //arguments
+            
+            self.excuteTask.terminationHandler = {
+                
+                task in
+                DispatchQueue.main.async(execute: {
+                    self.excuteButton.isEnabled = true
+                    self.spinner.stopAnimation(self)
+                    self.isRunning = false
+                })
+                
+            }
+            
+            self.captureStandardOutputAndRouteToTextView(self.excuteTask)
+            self.excuteTask.launch()
+            self.excuteTask.waitUntilExit()
+            
+        }
+    }
+    
+    func captureStandardOutputAndRouteToTextView(_ task:Process) {
+        
+        outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        
+        outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.NSFileHandleDataAvailable, object: outputPipe.fileHandleForReading , queue: nil) {
+            notification in
+
+            let output = self.outputPipe.fileHandleForReading.availableData
+            let outputString = String(data: output, encoding: String.Encoding.utf8) ?? ""
+
+            DispatchQueue.main.async(execute: {
+                let previousOutput = self.outputText.string ?? ""
+                let nextOutput = previousOutput + "\n" + outputString
+                self.outputText.string = nextOutput
+                
+                let range = NSRange(location:nextOutput.characters.count,length:0)
+                self.outputText.scrollRangeToVisible(range)
+                
+            })
+            
+            self.outputPipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+            
+        }
+    }
 }
 
